63f8972d8db7bcf7e46cc54b24951dc382fe4eea
Fishy Ofixial <ivanrdlt47@gmail.com>
2024-12-18 11:33:36 -0600

    Persistencia de Cuenta

--- screens/firebase.js
+++ screens/firebase.js
@@ -1,6 +1,11 @@
-import { initializeApp } from 'firebase/app';
+import { initializeApp, getApps } from 'firebase/app';
 import { getDatabase, ref, get, set, push } from 'firebase/database';
-import { getAuth } from 'firebase/auth';
+import { 
+    getAuth, 
+    initializeAuth, 
+    getReactNativePersistence 
+} from 'firebase/auth';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { API_KEY, AUTH_DOMAIN } from '@env';
 
 const firebaseConfig = {
@@ -11,11 +16,15 @@ const firebaseConfig = {
     messagingSenderId: "289829473388",
     appId: "1:289829473388:web:bcedd3ab00f9ffeebd5c49",
     measurementId: "G-8P4E09TSYB",
-    databaseURL: "https://walkingbuddy-f8712-default-rtdb.firebaseio.com/" 
+    databaseURL: "https://walkingbuddy-f8712-default-rtdb.firebaseio.com/"
 };
 
-const app = initializeApp(firebaseConfig);
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
+
 const database = getDatabase(app);
-const auth = getAuth(app);
+
+const auth = initializeAuth(app, {
+    persistence: getReactNativePersistence(AsyncStorage)
+});
 
 export { database, ref, get, set, push, auth };
